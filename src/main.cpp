fd406a92173c799dcf70e33c28b51079ddd7f5e0
Jan Řehoř <rehorja8@fit.cvut.cz>
2024-05-04 17:35:03 +0200

    Add cpr library to Windows via dll files

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,8 @@
  *  spakoro1
  */
 #include <string>
+#include <iostream>
+#include <cpr/cpr.h>
 #include "Application.hpp"
 void parseArguments(int argc, char** argv, std::string& mqttHostname, int& mqttPort) {
     if (argc > 1) {
@@ -29,14 +31,30 @@ void parseArguments(int argc, char** argv, std::string& mqttHostname, int& mqttP
 }
 
 int main(int argc, char** argv) {
-    // these default values will be used only if the user does not enter different values as command line arguments
-    std::string mqttHostname = "localhost";
-    int mqttPort = 1883;
-
-    parseArguments(argc, argv, mqttHostname, mqttPort);
-
-    Application application;
-    application.Initialize(mqttHostname, mqttPort);
-    application.Run();
+    
+    // TODO - remove: this is for a test
+    std::cout << "Testing cpr library, type enter..." << std::endl;
+    getchar();
+    
+    cpr::Response r = cpr::Get(cpr::Url{"https://api.github.com/repos/whoshuu/cpr/contributors"},
+                               cpr::Authentication{"user", "pass", cpr::AuthMode::BASIC},
+                               cpr::Parameters{{"anon", "true"}, {"key", "value"}});
+    std::cout << "status code: " << r.status_code << "\n";                  // 200
+    std::cout << "content type:" << r.header["content-type"] << "\n";       // application/json; charset=utf-8
+    std::cout << "content:\n " << r.text << "\n";                         // JSON text string
+    std::cout << "waiting for enter..." << std::endl;
+    getchar();
+    
+    // TODO: un-comment this
+//    // these default values will be used only if the user does not enter different values as command line arguments
+//    std::string mqttHostname = "localhost";
+//    int mqttPort = 1883;
+//
+//    parseArguments(argc, argv, mqttHostname, mqttPort);
+//
+//    Application application;
+//    application.Initialize(mqttHostname, mqttPort);
+//    application.Run();
+    
     return 0;
 }
