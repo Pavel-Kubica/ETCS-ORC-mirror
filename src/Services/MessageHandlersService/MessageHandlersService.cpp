b661af551f7ceb883fa29fdd8c0145aa5ee4db7c
Anna Pavlíková <pavlian5@fit.cvut.cz>
2024-05-08 14:22:31 +0200

    Change MqttListener and MessageHandlersService to handle LPCtoORC topic and messages

--- src/Services/MessageHandlersService/MessageHandlersService.cpp
+++ src/Services/MessageHandlersService/MessageHandlersService.cpp
@@ -10,11 +10,14 @@
  *  ### Contributors
  *  veselo21
  *  zimaluk1
+ *  pavlian5
  */
 
 #include "MessageHandlersService.hpp"
+#include "SimulationStatusMessageHandler.hpp"
 
 void MessageHandlersService::Initialize(ServiceContainer& container) {
+    messageHandlers[MessageID::SimulationStatusLPCtoORC] = std::make_shared<SimulationStatusMessageHandler>(container);
 }
 
 std::map<MessageID, std::shared_ptr<MessageHandler>> MessageHandlersService::GetAllHandlers() const {
