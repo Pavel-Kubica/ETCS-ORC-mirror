ef8f1becd391db4eee3d954d1e4e5dcd13c8891b
Jan Řehoř <rehorja8@fit.cvut.cz>
2024-05-12 16:46:33 +0200

    Add more descriptive error message when error occures while parsing balises.json

--- src/Services/BalisesFromJson/BalisesFromJsonService.cpp
+++ src/Services/BalisesFromJson/BalisesFromJsonService.cpp
@@ -62,7 +62,7 @@ std::vector<std::unique_ptr<Balise>> BalisesFromJsonService::GetBalisesFromJson(
         baliseJson = nlohmann::json::parse(baliseFile);
     } catch (const nlohmann::json::exception& e) {
         jruLoggerService->Log(true, MessageType::Error,
-                              "BalisesFromJsonService: Error while parsing balise file: " + std::string(e.what()));
+                              R"(BalisesFromJsonService: Error "%error%" while parsing the balise file: "%file%")", e.what(), pathToFile);
         return {};
     }
     std::vector<std::unique_ptr<Balise>> balises;
