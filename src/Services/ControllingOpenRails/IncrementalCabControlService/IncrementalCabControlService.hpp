64e165a73047d67f5677b9ea36b0f653030de752
Jan Řehoř <rehorja8@fit.cvut.cz>
2024-05-13 18:13:09 +0200

    Reformat header file

--- src/Services/ControllingOpenRails/IncrementalCabControlService/IncrementalCabControlService.hpp
+++ src/Services/ControllingOpenRails/IncrementalCabControlService/IncrementalCabControlService.hpp
@@ -14,79 +14,78 @@
 
 #pragma once
 
-#include "IIncrementalCabControlService.hpp"
 #include "ConfigurationService.hpp"
-#include "IInitializable.hpp"
-#include "ILpcManageable.hpp"
-#include "ILocalCabControlsDataService.hpp"
 #include "ICabControlApiService.hpp"
+#include "IIncrementalCabControlService.hpp"
+#include "IInitializable.hpp"
+#include "ILocalCabControlsDataService.hpp"
+#include "ILpcManageable.hpp"
 
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <thread>
 #include "AsyncProperty.hpp"
 #include "Increment.hpp"
 #include "IncrementalCabControlConfiguration.hpp"
-#include <chrono>
-#include <atomic>
-#include <thread>
-#include <condition_variable>
 
 class IncrementalCabControlService : public IInitializable,
                                      public ILpcManageable,
                                      public IIncrementalCabControlService {
 protected:
     void Initialize(ServiceContainer& container) override;
+
 public:
     void StartIncreasingThrottle() override;
     void StopChangingThrottle() override;
     void StartDecreasingThrottle() override;
-    
+    void SetThrottleTo(double value) override;
+
     void StartIncreasingDynamicBrake() override;
     void StopChangingDynamicBrake() override;
     void StartDecreasingDynamicBrake() override;
-    void SetThrottleTo(double value) override;
     void SetDynamicBrakeTo(double value) override;
-    bool LpcSaidStart() override;
-    
-    bool LpcSaidStop() override;
-    
-    bool LpcSaidRestart() override;
 
+    bool LpcSaidStart() override;
+    bool LpcSaidStop() override;
+    bool LpcSaidRestart() override;
 
 private:
     ICabControlApiService* cabControlApiService;
     ILocalCabControlsDataService* localCabControlsDataService;
-    
+
     Increment throttleIncrement;
     Increment brakeIncrement;
-    
+
     // Loading from config files
     IncrementalCabControlConfiguration config;
     ConfigurationService* configurationService;
-    
+
     // Says if the incrementing thread should be running or not
     std::atomic_bool shouldRun;
     std::mutex mtx;
     std::condition_variable cv;
     std::thread incrementingThread;
-    
+
     void IncrementingThreadEntryPoint();
-    
+
     /**
      * @warning THIS METHOD IS NO SYNCHRONISED - IT DOES NOT LOCK `this->mtx`
-     * @param [in, out] request - request that this method adds throttle changes to, which should be sent to Open Rails API
+     * @param [in, out] request - request that this method adds throttle changes to, which should be sent to Open Rails
+     * API
      */
     void ChangeThrottle(CabControlRequest& request);
-    
+
     /**
      * @warning THIS METHOD IS NO SYNCHRONISED - IT DOES NOT LOCK `this->mtx`
      * @param [in, out] request - request that this method adds brake changes to, which should be sent to Open Rails API
      */
     void ChangeBrake(CabControlRequest& request);
-    
+
     /**
      * @warning THIS METHOD IS NO SYNCHRONISED - IT DOES NOT LOCK `this->mtx`
      * @return true if incrementing thread has no more work to do
      * (meaning `this->throttleIncrement` and `this->brakeIncrement` are `Increment::None`).
      */
     [[nodiscard]] bool WorkDone() const;
-    
 };
